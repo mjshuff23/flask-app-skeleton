eaaf85bb9fb9287542a9cfc6fea303b51c9a34f6
Michael Shuff <mjshuff23@gmail.com>
2020-11-13 00:08:56 -0500

    working

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from config import Config
+from .routes import index
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-from app import routes
+app.register_blueprint(index.bp)
